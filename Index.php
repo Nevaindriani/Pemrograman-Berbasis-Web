5dab0adf8aa9b3904a6a6421f889af10811920ab
Nevaindriani <73055095+Nevaindriani@users.noreply.github.com>
2020-12-24 21:22:35 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,13 @@
+<html>
+<table>
+<form method="post" action="Percabangan.php">
+	<tr>
+		<td>Masukkan jumlah Pemakaian air</td>
+		<td><input type="text" name="guna"/></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td><input type="submit" name="proses" value="HITUNG" /></td>
+	</tr>
+</table>
+</html>
